2f11ff9e891f3ac5075c84be107b3220d18a2459
kdeng00 <kundeng00@pm.me>
2025-06-10 22:44:38 +0000

    Fetch queue item (#20)
    Reviewed-on: https://git.kundeng.us/phoenix/songparser/pulls/20
    Co-authored-by: kdeng00 <kundeng00@pm.me>
    Co-committed-by: kdeng00 <kundeng00@pm.me>

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
-// use std::error::Error;
-// use tokio::io::AsyncReadExt;
-// use tokio::net::{TcpListener, TcpStream};
-// use tokio::spawn;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
     let app_base_url = get_icarus_url().await;
 
     loop {
         println!("Base URL: {}", app_base_url);
 
-        // TODO: Update the api/v2/song/queue/next endpoint to only retrieve queued song that is
-        // ready to be processed. Make necessary changes to other endpoints
-
-        let api_url = format!("{}/api/v2/song/queue/next", app_base_url);
-
-        match client.get(api_url).send().await {
+        match api::fetch_next_queue_item(&app_base_url).await {
             Ok(response) => {
-                let body = response.text().await?;
-                println!("API response: {}", body);
-                // Process data here...
+                match response
+                    .json::<responses::fetch_next_queue_item::SongQueueItem>()
+                    .await
+                {
+                    Ok(song_queue_item) => {
+                        if !song_queue_item.data.is_empty() {
+                            println!("Song queue item: {:?}", song_queue_item);
 
-                // TODO: Parse the response body to a struct
-                // TODO: Get queued song data
-                // TODO: Get queued song's metadata
-                // TODO: Get queued coverart
-                // TODO: Get queued coverart's data
-                // TODO: Apply metadata to the queued song
-                // TODO: Update the queued song with the updated queued song
-                // TODO: Create song
-                // TODO: Create coverart
-                // TODO: Wipe data from queued song
-                // TODO: Wipe data from queued coverart
+                            // Process data here...
+
+                            // TODO: Parse the response body to a struct
+                            // TODO: Get queued song data
+                            // TODO: Get queued song's metadata
+                            // TODO: Get queued coverart
+                            // TODO: Get queued coverart's data
+                            // TODO: Apply metadata to the queued song
+                            // TODO: Update the queued song with the updated queued song
+                            // TODO: Create song
+                            // TODO: Create coverart
+                            // TODO: Wipe data from queued song
+                            // TODO: Wipe data from queued coverart
+                        } else {
+                            println!("No data to fetch");
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("Error: {:?}", err);
+                    }
+                }
             }
             Err(e) => eprintln!("API call failed: {}", e),
         }
@@ -40,7 +43,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Sleeping");
         tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
     }
-    // Ok(())
+}
+
+mod responses {
+    pub mod fetch_next_queue_item {
+        use serde::{Deserialize, Serialize};
+
+        #[derive(Debug, Deserialize, Serialize)]
+        pub struct QueueItem {
+            pub id: uuid::Uuid,
+            pub filename: String,
+            pub status: String,
+        }
+
+        #[derive(Debug, Deserialize, Serialize)]
+        pub struct SongQueueItem {
+            pub message: String,
+            pub data: Vec<QueueItem>,
+        }
+    }
+}
+
+mod api {
+    pub async fn fetch_next_queue_item(
+        base_url: &String,
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        let client = reqwest::Client::new();
+        let fetch_endpoint = String::from("api/v2/song/queue/next");
+        let api_url = format!("{}/{}", base_url, fetch_endpoint);
+        client.get(api_url).send().await
+    }
 }
 
 async fn get_icarus_url() -> String {
